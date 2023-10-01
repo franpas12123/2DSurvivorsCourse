d26165330309fb59d2734acbb4b6b989381209f3
Frances Pasulohan <franpas12123@gmail.com>
2023-10-02 00:59:18 +0800

    change delta to _delta

--- player/player.gd
+++ player/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
